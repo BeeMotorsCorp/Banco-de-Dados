58fd93bf777a759ef5f88b0dbcd51abd72c9cc47
gustavoworks138 <gustavodoprado09@gmail.com>
2025-11-10 13:55:05 -0300

    tabela produtos

---
+++ tabela produtos.sql
@@ -0,0 +1,17 @@
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    estoque INT NOT NULL,           
+    calibre VARCHAR(20),
+    capacidade VARCHAR(20),
+    peso VARCHAR(20),             
+    marca VARCHAR(50),
+    categoria VARCHAR(50),
+    badge VARCHAR(30),             
+    imagem VARCHAR(255),   
+    disponivel ENUM('Sim','Não') DEFAULT 'Sim',
+    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP, -- quando foi inserido
+    data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- ultima vez que foi atualizado
+);
